d48c34444f03e5e21b075b9507c4a7fa659e7932
Xue-boJin <32252255+Xue-boJin@users.noreply.github.com>
2020-04-01 08:53:27 +0800

    Update mydata.m

--- mydata.m
+++ mydata.m
@@ -1,10 +1,11 @@
 clear
 clc
-x(1)=5;
-for i=2:50
-    x(i)=0.7*x(i-1)+sqrt(1)*rand(1);
-    z1(i)=x(i-1)+sqrt(1)*rand(1);
-    z2(i)=3*x(i-1)+sqrt(3)*rand(1);
+x0=5;
+for i=1:50
+    x(i)=0.7*x0+sqrt(1)*rand(1);
+    z1(i)=x(i)+sqrt(1)*rand(1);
+    z2(i)=3*x(i)+sqrt(3)*rand(1);
+    x0=x(i);
 end
 
 plot(x)
